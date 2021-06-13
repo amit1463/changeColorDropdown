c69970b3e966087811debdaf097386c53ab7a339
Amit Kumar Mandal <itsme.amit1230@gmail.com>
2021-06-14 03:48:26 +0530

    change bcolor using dropdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
+
 function App() {
+ 
+  const [optionsState,setoptionsState] = useState("select color");
+
+  // const arr = [
+ 
+  //   {
+  //     colors:"red",
+  //   }
+
+  // ]
+
+  const [color,setColor] = useState("white");
+
+
+  const handleChange = (e) =>{
+    setoptionsState(e.target.value);
+    // console.log(setoptionsState);
+    console.log(optionsState);
+
+    if(e.target.value==="violet"){
+      setColor("violet");
+    }
+    else if(e.target.value==="indigo"){
+      setColor("indigo");
+    }
+    else if(e.target.value==="blue"){
+      setColor("blue");
+    }
+    else if(e.target.value==="green"){
+      setColor("green");
+    }
+    else if(e.target.value==="yellow"){
+      setColor("yellow");
+    }
+    else if(e.target.value==="orange"){
+      setColor("orange");
+    }
+    else if(e.target.value==="red"){
+      setColor("red");
+    }
+    else{
+      setColor("white");
+    }
+    
+  }
+
+  // const handleclick = ()=>{
+
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{background:color}}>
+      <h2>change background color using below<br/> <span>Dropdown <span className="vib">{"VIBGYOR"}</span> List</span></h2>
+      <select value={optionsState} onChange={handleChange}>
+
+        <option value="">select color</option>
+        <option value="violet">Violet</option>
+        <option value="indigo">Indigo</option>
+        <option value="blue">Blue</option>
+        <option value="green">Green</option>
+        <option value="yellow">Yellow</option>
+        <option value="orange">Orange</option>
+        <option value="red">Red</option>
+      </select>
+      
     </div>
   );
 }
